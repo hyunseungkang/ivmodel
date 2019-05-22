65152ded0956a847f2089309329c447db5c4d6ab
qingyuanzhao <qingyzhao@gmail.com>
2019-05-21 23:32:02 -0400

    Output stand.diff for binary Z and any X

--- R/barplot.R
+++ R/barplot.R
@@ -48,15 +48,15 @@ iv.diagnosis <- function(Y, D, Z, X) {
         return(t(apply(X, 2, function(X) unlist(iv.diagnosis(Y, D, Z, X)))))
     }
 
-    flag <- FALSE
     if (length(unique(X)) == 2 & length(unique(Z)) == 2) {
         p.val <- fisher.test(X, Z)$p.value
-        stand.diff <- (mean(X[Z==1])-mean(X[Z==0]))/(sqrt((var(X[Z==1])+var(X[Z==0]))/2))
-        flag <- TRUE
     } else if (length(unique(X)) == 2) {
         p.val <- summary(glm(X ~ Z, family = "binomial"))$coefficients[2, 4]
     } else {
         p.val <- summary(lm(X ~ Z))$coefficients[2, 4]
+    }
+    if (length(unique(Z)) == 2) {
+        stand.diff <- (mean(X[Z==1])-mean(X[Z==0]))/(sqrt((var(X[Z==1])+var(X[Z==0]))/2))
     }
     prev.diff.ratio <- (lm(X ~ Z)$coef[2])/(lm(X ~ D)$coef[2]);
     bias.ratio <- prev.diff.ratio / (lm(D ~ Z)$coef[2])
@@ -72,7 +72,7 @@ iv.diagnosis <- function(Y, D, Z, X) {
         output <- list(x.mean1 = mean(X[Z == 1]),
                        x.mean0 = mean(X[Z == 0]),
                        p.val = p.val,
-                       stand.diff = ifelse(flag, stand.diff, NA),
+                       stand.diff = stand.diff,
                        bias.ratio = bias.ratio,
                        bias.amplify = bias.amplify,
                        bias.ols = bias.ols,
