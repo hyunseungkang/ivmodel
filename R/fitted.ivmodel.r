e60a0d5655f784fa6f6725d51c83a791090f794e
Hyunseung Kang <22965055+hyunseungkang@users.noreply.github.com>
2021-01-06 16:06:43 -0600

    fixed bug in fitted. simplified code relying on resid()

--- R/fitted.ivmodel.r
+++ R/fitted.ivmodel.r
@@ -1,30 +1,9 @@
 fitted.ivmodel <- function(object, ...){
   ivmodel <- object
-  temp <- matrix(NA, nrow=2, ncol=0)
-  if(!is.null(ivmodel$kClass)){
-    temp<-rbind(as.numeric(rownames(ivmodel$kClass$ci)), c(ivmodel$kClass$point.est))
-	colnames(temp) <- paste("k=", temp[1,], sep="")
-    colnames(temp)[temp[1,]==0] <- "OLS"
-    colnames(temp)[temp[1,]==1] <- "TSLS"
-  }
-  if(!is.null(ivmodel$LIML)){  
-    LIML <- c(ivmodel$LIML$k, ivmodel$LIML$point.est)
-	temp <- cbind(temp, LIML)
-  }
-  if(!is.null(ivmodel$Fuller)){  
-    Fuller <- c(ivmodel$Fuller$k, ivmodel$Fuller$point.est)
-	temp <- cbind(temp, Fuller)
-  }
-
-  result <- matrix(NA, ncol=ncol(temp), nrow=ivmodel$n)
-  colnames(result) <- colnames(temp)
-  for(i in 1:ncol(temp)){
-    result[, i] <- ivmodel$Yadj + temp[2, i]*(ivmodel$D-ivmodel$Dadj)
-  }
-
-  # NA handling  
-  output = matrix(NA, ncol=ncol(temp), nrow=length(ivmodel$naindex))
-  colnames(output) <- colnames(temp)
-  output[which(ivmodel$naindex), ] = result
-  return(output)
+  residual_est =  resid(ivmodel)
+  stopifnot("Number of estimated residuals do not equal number of outcome"= nrow(residual_est) == nrow(ivmodel$Y))
+  
+  result = matrix(ivmodel$Y,nrow(residual_est),ncol(residual_est)) - residual_est
+  result = result[which(ivmodel$naindex),]
+  return(result)
 }
